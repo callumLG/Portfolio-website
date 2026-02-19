cd7a8358bedf28e7e8ea5d1b5efa8dad512b1120
Callum Govender <callumGovender06@gmail.com>
2026-02-19 17:54:56 +0200

    created buttons in hero sec

--- portfolio/app/components/Greet.jsx
+++ portfolio/app/components/Greet.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Greet() {
-  return <div>Home</div>;
+  return (
+    <div className="text-center py-12 flex flex-col gap-4">
+      <h1 className="text-4xl font-bold text-white">Callum Govender</h1>
+      <p className="text-lg text-gray-300 mt-4">
+        Junior Web Developer / Frontend Developer
+      </p>
+      <div className=" flex gap-2.5 mx-auto">
+        <button className="text-[#A8C5E8] w-50 border-2 rounded-lg py-3 hover:bg-[#A8C5E8] hover:text-black transition duration-300">
+          View Projects
+        </button>
+        <button className="text-[#F5A3A3] w-50 border-2 rounded-lg py-3  hover:bg-[#F5A3A3] hover:text-black transition duration-300">
+          CV
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Greet;
